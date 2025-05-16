5b76c04c0a7e07fa9bd7a36a7029284e4f1c2fbe
Whitney Marlier <piinkswanoff@gmail.cim>
2025-05-16 12:29:35 +0200

    site fini

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector(".header");
+    const btn = document.querySelector("#btn");
+
+    btn.addEventListener("click", () => {
+      header.classList.toggle("show-nav");
+    });
+  });
